73cf9323f0a88659d5e519bc72037d6d7a5582fd
legalvincent056 <legalvincent056@gmail.com>
2017-04-27 22:23:14 +0200

    Implementation de retourneClientParId dans la dao +tests associés

---
+++ proxibanque/src/test/java/test/dao/retourneClientParId.java
@@ -0,0 +1,39 @@
+package test.dao;
+
+import static org.junit.Assert.*;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import dao.DaoImpl;
+import dao.IDao;
+import metier.Client;
+
+public class retourneClientParId {
+
+	EntityManagerFactory emf = Persistence.createEntityManagerFactory("proxiv3-pu");
+
+	@Test
+	public void testRetourneUnClient() {
+		IDao dao = new DaoImpl();
+
+		Client c = dao.retourneClientParId(2);
+		
+		Assert.assertEquals("ZEZE", c.getNom());
+	}
+	
+	@Test
+	public void testRetourneUnClientAvecSonAdresse() {
+		IDao dao = new DaoImpl();
+
+		Client c = dao.retourneClientParId(2);
+		
+		Assert.assertEquals("GENAS", c.getAdresse().getVille());
+	}
+	
+
+}
